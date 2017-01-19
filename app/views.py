8de477545ac4365815c8126253c62210f6094f21
hariprabha92 <hariprabha.haridas@gmail.com>
2017-01-19 20:08:42 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login
+from django.views.generic import View
+from django.http import HttpResponse
+#from django import forms
+from .forms import UserForm
+def home(request):
+    
+    return render(request, 'app/index.html')
+
+def register(request):
+   # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            password2 = form.cleaned_data['re_enter_password']
+            if password == password2:
+                user.set_password(password)
+                user.save()
+                user.backend = 'django.contrib.auth.backends.ModelBackend'
+                login(request, user)
+                return render(request, 'app/index.html')
+            else:
+                HttpResponse('Password must match')
+                return render(request, 'app/register.html', {'form': form})
+
+    else:
+        form = UserForm()
+    return render(request, 'app/register.html', {'form': form})
+
